470469ce0c7a626644e05c5f057b49a8387d057f
parasf <email@youremail.com>
2021-05-30 00:25:10 +0530

    ch

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var client_id = '3017e66dd65045b0bb6423b18d588a18'
 var client_secret = '683296dcce3140418ebfe33cd505da20'
-var redirect_uri = 'https://tender-johnson-06acee.netlify.app/'
+var redirect_uri = 'https:%2F%2Ftender-johnson-06acee.netlify.app%2F'
 var scopes = 'user-read-private user-read-email user-modify-playback-state user-read-playback-position user-libraaming user-read-playback-state user-read-recently-played playlist-read-private'
 var auth='https://accounts.spotify.com/authorize'
 

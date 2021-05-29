c61f0cc9921bbcb96d2d85977de5751134555dfd
parasf <email@youremail.com>
2021-05-30 00:09:51 +0530

    authen

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 var client_id = '3017e66dd65045b0bb6423b18d588a18'
-var client_secret='683296dcce3140418ebfe33cd505da20'
+var client_secret = '683296dcce3140418ebfe33cd505da20'
+var redirect_uri = 'https://tender-johnson-06acee.netlify.app/'
+var scopes = 'user-read-private user-read-email user-modify-playback-state user-read-playback-position user-libraaming user-read-playback-state user-read-recently-played playlist-read-private'
+var auth='https://accounts.spotify.com/authorize'
+
+async function Authenticate() {
+    try {
+        var respj = await fetch(auth + '?client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=' + scopes +
+            '&response_type=token', { method: 'GET' })
+        var res = await respj.json();
+        console.log(res.access_token);
+        
+    } catch (error) {
+        console.log(error);
+    }
+   
+}
+Authenticate();
